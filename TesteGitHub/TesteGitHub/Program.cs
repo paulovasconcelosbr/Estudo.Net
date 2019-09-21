8d7640c0d3ced2e8bd952c0ad5f643218b1a9f33
paulo <paulovasconcelosbr@gmail.com>
2019-09-20 22:15:03 -0300

    estudo 2

--- TesteGitHub/TesteGitHub/Program.cs
+++ TesteGitHub/TesteGitHub/Program.cs
@@ -6,7 +6,7 @@ namespace TesteGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá Mundo!");
         }
     }
 }
